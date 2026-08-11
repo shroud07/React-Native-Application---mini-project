0a4ec52a6e89cf7feffa0ddf046441c88133977d
shroud07 <shusan4517@gmail.com>
2026-08-11 17:14:34 +0530

    Finished building Sign-In Page with Mfa feature

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,7 +1,164 @@
-import { router } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { codeSchema, SignInFormValues, signInSchema } from "@/lib/schemas/auth";
+import { useSignIn } from "@clerk/expo";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useRouter } from "expo-router";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { ActivityIndicator, Image, KeyboardAvoidingView, Platform, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+
 
 export default function SignInScreen() {
+
+    const { signIn, errors, fetchStatus } = useSignIn();
+    const router = useRouter();
+    const isLoading = fetchStatus === "fetching";
+
+    const {
+            control,
+            handleSubmit,
+            formState: { errors: formErrors },
+        } = useForm<SignInFormValues>({resolver : zodResolver(signInSchema), 
+            mode: "onBlur",
+            defaultValues: { email: "", password: "" },
+        });
+    
+    const {
+        control: codeControl,
+        handleSubmit: handleCodeSubmit,
+        formState: { errors: codeFormErrors },
+    } = useForm<{code: string}>({resolver : zodResolver(codeSchema), 
+        mode: "onBlur",
+        defaultValues: { code: "" },
+    });
+
+
+    const onSignInPress = async (values : SignInFormValues) => {
+    
+            const {error} = await signIn.password({
+                emailAddress: values.email,
+                password: values.password,
+            });
+            if(error) return;
+
+            if(signIn.status === "complete"){
+                await signIn.finalize({
+                    navigate: ({session, decorateUrl}) => {
+                        if(session?.currentTask) return;
+                        const url = decorateUrl("/");
+                        router.replace(url as any);
+                    },
+                });
+            } else if(signIn.status === "needs_second_factor"){
+                await signIn.mfa.sendPhoneCode();
+            } else if(signIn.status === "needs_client_trust"){
+                const emailCodefactor = signIn.supportedSecondFactors.find(
+                    (factor) => factor.strategy === "email_code",
+                );
+                if (emailCodefactor) {
+                    await signIn.mfa.sendEmailCode();
+                }
+            } else{
+                console.error("Sign In attempt not complete: ", signIn);
+            }
+        };
+
+        const onVerifyPress = async({code} : {code : string}) => {
+        await signIn.mfa.verifyEmailCode({code});
+
+        if(signIn.status === "complete"){
+            await signIn.finalize({
+                navigate: ({session, decorateUrl}) => {
+                    if(session?.currentTask) return;
+                    const url = decorateUrl("/");
+                    router.replace(url as any);
+                },
+            });
+        } else{
+            console.error("Sign-up attempt not complete:", signIn);
+        }
+    };
+
+    if(signIn.status === "needs_client_trust"){
+        return(
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                    className="flex-1 bg-brand-body"
+                >
+                    <View
+                        className="flex-1 justify-center px-6 -mt-16">
+        
+                        <Image
+                            source= {require("../../assets/images/welth.png")}
+                            className = "w-36 h-16 mb-8"
+                            resizeMode = "contain"
+                        />
+        
+                        <Text
+                            className="text-3xl font-bold text-[#1A1D26] mb-2 leading-tight"
+                            >
+                                Verify Your Account
+                        </Text>
+        
+                        <Controller
+                                control={codeControl}
+                                name="code"
+                                render={({field: {value, onChange}}) => {
+                                    return <TextInput
+                                        className="border border-[#E8E6DF] bg-white
+                                        rounded-xl px-4 py-3 mb-2 text-[#1A1D26]"
+                                        placeholder="Enter Verification Code"
+                                        placeholderTextColor="#8A8D96"
+                                        value = {value}
+                                        onChangeText={onChange}
+                                        
+                                        />
+                                }}>
+                                    
+                        </Controller>
+                        {(codeFormErrors.code) && (
+                            <Text className="text-brand-coral mb-4 text-sm">
+                                {codeFormErrors.code?.message}
+                            </Text>
+                        )}
+                        {errors.fields.code && (
+                            <Text className = "text-brand-coral mb-4 text-sm">
+                                {errors.fields.code.message}
+                            </Text>
+                        )}
+        
+                        <TouchableOpacity
+                            onPress={handleCodeSubmit(onVerifyPress)}
+                            disabled = {isLoading}
+                            className="w-full bg-brand-blue py-4 rounded-xl items-center mb-4">
+                            {isLoading ? (
+                                <ActivityIndicator color="white"/>
+                            ) : (
+                                <Text className="text-white font-semibold text-base">
+                                    Verify
+                                </Text>
+                            )}
+                        </TouchableOpacity>
+        
+                        <TouchableOpacity
+                            onPress={() => signIn.mfa.sendEmailCode()}
+                            className="py-2">
+                                <Text className="text-brand-blue text-sm">
+                                    I need a new code
+                                </Text>
+                        </TouchableOpacity>
+                        
+                        <TouchableOpacity
+                            onPress={() => signIn.reset()}
+                            className="py-2">
+                                <Text className="text-brand-blue text-sm">
+                                    Start Over
+                                </Text>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
+                )
+    }
+
     const handleSignIn = () => {
         // Later:
         // await login(email, password)
@@ -10,25 +167,115 @@ export default function SignInScreen() {
     };
 
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                gap: 20,
-            }}
+        <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex-1 bg-brand-body"
         >
-            <Text>Sign In</Text>
-
-            <Pressable onPress={handleSignIn}>
-                <Text>Sign In</Text>
-            </Pressable>
-
-            <Pressable
-                onPress={() => router.push("/sign-up")}
+            <ScrollView
+                contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+                keyboardShouldPersistTaps="handled"
+                showsVerticalScrollIndicator={false}
+                className="flex-1 px-6 -mt-16"
             >
-                <Text>Create Account</Text>
-            </Pressable>
-        </View>
+
+                <Image
+                    source= {require("../../assets/images/welth.png")}
+                    className = "w-36 h-16 mb-8"
+                    resizeMode = "contain"  
+                />
+
+                <Text
+                    className="text-3xl font-bold text-[#1A1D26] mb-2 leading-tight"
+                    >
+                        Welcome Back
+                </Text>
+                <Text
+                    className="text-brnad-text-muted text-base mb-8">
+                        Sign in to your account
+                </Text>
+
+                <Controller
+                        control={control}
+                        name="email"
+                        render={({field: {value, onChange}}) => {
+                            return <TextInput
+                                className="border border-[#E8E6DF] bg-white
+                                rounded-xl px-4 py-3 mb-2 text-[#1A1D26]"
+                                placeholder="Email Address"
+                                placeholderTextColor="#8A8D96"
+                                value = {value}
+                                onChangeText={onChange}
+                                />
+                        }}>
+                            
+                </Controller>
+                {(formErrors.email) && (
+                    <Text className="text-brand-coral mb-4 test-sm">
+                        {formErrors.email?.message}
+                    </Text>
+                )}
+                {errors.fields.identifier && (
+                    <Text className = "text-brand-coral mb-4 text-sm">
+                        {errors.fields.identifier.message}
+                    </Text>
+                )}
+
+                <Controller
+                        control={control}
+                        name="password"
+                        render={({field: {value, onChange}}) => {
+                            return <TextInput
+                                className="border border-[#E8E6DF] bg-white
+                                rounded-xl px-4 py-3 mb-5 text-[#1A1D26]"
+                                placeholder="Password"
+                                placeholderTextColor="#8A8D96"
+                                value = {value}
+                                onChangeText={onChange}
+                                autoCapitalize="words"
+                                secureTextEntry
+                                />
+                        }}>
+                            
+                </Controller>
+                {(formErrors.password) && (
+                    <Text className="text-brand-coral mb-4 test-sm">
+                        {formErrors.password?.message}
+                    </Text>
+                )}
+                {errors.fields.password && (
+                    <Text className = "text-brand-coral mb-4 text-sm">
+                        {errors.fields.password.message}
+                    </Text>
+                )}
+
+                <TouchableOpacity
+                    onPress={handleSubmit(onSignInPress)}
+                    disabled = {isLoading}
+                    className="w-full bg-brand-blue py-4 rounded-xl items-center mb-4">
+                    {isLoading ? (
+                        <ActivityIndicator color="white"/>
+                    ) : (
+                        <Text className="text-white font-semibold text-base">
+                            Sign In
+                        </Text>
+                    )}
+                </TouchableOpacity>
+
+                <View
+                    className="flex-row justify-center">
+                        <Text 
+                            className="text-brand-text-muted">
+                            Don't have an account?{" "}
+                        </Text>
+                        <Link
+                            href = "/sign-up">
+                                <Text className="text-brand-blue font-semibold">
+                                    Sign Up
+                                </Text>
+                        </Link>
+                </View>
+
+             </ScrollView>    
+        </KeyboardAvoidingView>
     );
 }
